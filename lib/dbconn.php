a00bfd50bfc8083296cfad5693c6e4ecb5ff20d1
gurals0329 <gurals0329@naver.com>
2020-11-09 15:42:15 +0900

    dbconn.php 파일수정

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -1,5 +1,5 @@
 <?
-    $connect=mysql_connect( "localhost", "gurals0329", "wldnjs22@") or  
+    $connect=mysql_connect( "localhost", "", "") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
     mysql_select_db("gurals0329",$connect);
